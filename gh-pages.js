b1946729492e4e44df608e87a11e53980537c925
Kriss Watt <kriss.watt@gmail.com>
2022-03-24 20:59:48 -0700

    add gh-pages

---
+++ gh-pages.js
@@ -0,0 +1,14 @@
+import { ghpages } from 'gh-pages';
+
+ghpages.publish(
+    'public',
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/voodoochild/baldathon-timer.git',
+        user: {
+            name: 'Kriss Watt',
+            email: 'kriss.watt@gmail.com',
+        }
+    },
+    () => console.log('Deployed to Github Pages')
+);
